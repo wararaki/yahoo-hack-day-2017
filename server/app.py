4857a2b8b40f282dbd9f77f79a19abbe8511fc25
wararaki <>
2017-12-09 21:00:46 +0900

    add server application

---
+++ server/app.py
@@ -0,0 +1,27 @@
+'''
+sample application
+'''
+
+from flask import Flask
+
+app = Flask("sample")
+
+@app.route("/sample")
+def sample():
+    '''
+    sample output
+    '''
+    return '''
+            <html>
+                <head>
+                </head>
+                <body>
+                    <div>
+                        Hello, World!
+                    </div>
+                </body>
+            </html>
+           '''
+
+if __name_ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
